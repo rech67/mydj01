da242270a0018fe4f2623ae5e16fac124489696d
rech67 UX <rech67@gmail.com>
2021-05-04 15:48:58 +0800

    ex fix

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-<<<<<<< HEAD
+
 def hello(request):
-=======
-def hello():
->>>>>>> a6c177ceb4ba45778ef5e743224e01a30f41430a
     return HttpResponse('<h2>哈摟!!</h2>')
